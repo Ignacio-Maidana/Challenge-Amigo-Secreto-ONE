8e3a480e26a7b09c5e00647c45e1731b972b77fc
Ignacio Maidana <ignacio.maidana99@gmail.com>
2025-08-01 16:13:15 -0300

    Challenge finalizado

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    if (nuevoAmigo === '') {
+        alert('Por favor ingrese un nombre');
+    } else {
+        amigos.push(nuevoAmigo);
+    }
+
+    document.getElementById('amigo').value = '';
+
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i=0;i<amigos.length;i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if(amigos.length == 0 || amigos.length == 1) {
+        alert('Por favor, ingrese al menos 2 nombres');
+        return;
+    }
+
+    let i = Math.floor(Math.random() * amigos.length);
+
+    let sorteado = document.getElementById('resultado');
+    sorteado.innerHTML = `El amigo secreto sorteado es: ${amigos[i]}`;
+}
